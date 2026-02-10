1986432facfb3e4c6101cdfb38c6d6e54791002f
DielmeRamosGomes <dielme.ramos@unesp.br>
2026-02-10 14:53:22 -0300

    s1a1

---
+++ src/primeiro-bimestre/logica-e-linguagem-de-programacao/semana-01/tipo-variaveis.py
@@ -0,0 +1,12 @@
+
+letra = 'a'
+print(f"O tipo da variável letra é: {type(letra)}")
+
+idade = 25
+print(f"O tipo da variável idade é: {type(idade)}")
+
+media = 8.5
+print(f"O tipo da variável media é: {type(media)}")
+
+vai_de_onibus = True
+print(f"O tipo da variável vai_de_onibus é: {type(vai_de_onibus)}")
