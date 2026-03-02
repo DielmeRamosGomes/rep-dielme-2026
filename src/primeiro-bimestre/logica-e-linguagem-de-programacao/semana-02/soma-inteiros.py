c81d83ec1ebd1e16946e3979a9fd11a778b36a07
DielmeRamosGomes <dielme.ramos@unesp.br>
2026-03-02 08:56:30 -0300

    mudança

--- src/primeiro-bimestre/logica-e-linguagem-de-programacao/semana-02/soma-inteiros.py
+++ src/primeiro-bimestre/logica-e-linguagem-de-programacao/semana-02/soma-inteiros.py
@@ -1,5 +1,5 @@
-conta_total = 150
-num_pessoas = 4
+conta_total = 2
+num_pessoas = 0.5
 valor_por_pessoa = conta_total / num_pessoas
 print(f"O valor por pessoa é R$ {valor_por_pessoa:.2f} reais.")
 
