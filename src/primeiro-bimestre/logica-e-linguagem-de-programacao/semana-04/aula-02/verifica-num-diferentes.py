318aecdc741684e6f777655c4ff6208bd50331be
DielmeRamosGomes <dielme.ramos@unesp.br>
2026-03-11 08:59:19 -0300

    llp

--- src/primeiro-bimestre/logica-e-linguagem-de-programacao/semana-04/aula-02/verifica-num-diferentes.py
+++ src/primeiro-bimestre/logica-e-linguagem-de-programacao/semana-04/aula-02/verifica-num-diferentes.py
@@ -1,7 +1,13 @@
-num1 = int(input("Digite o primeiro número: "))
-num2 = int(input("Digite o segundo número: "))
-if num1 != num2:
-    print("Os números são diferentes.")
-else:
-    print("Os números são iguais.")
+def verifica_numero():
+    while True:
+        num1 = int(input("Digite o primeiro número: "))
+        num2 = int(input("Digite o segundo número: "))
+        if num1 != num2:
+            print("Os números são diferentes.")
+        else:
+            print("Os números são iguais.")
+        continua = input("Deseja continuar executando? [sim, nao]: ").lower()
+        if continua == "nao":
+            break
 
+verifica_numero()
