547d7e8751168d9098db2800e2cb2edd7169f596
DielmeRamosGomes <dielme.ramos@unesp.br>
2026-03-11 09:07:40 -0300

    llp

--- src/primeiro-bimestre/logica-e-linguagem-de-programacao/semana-04/aula-02/verifica-num-positivo.py
+++ src/primeiro-bimestre/logica-e-linguagem-de-programacao/semana-04/aula-02/verifica-num-positivo.py
@@ -1,13 +1,16 @@
-numero = float(input("Digite um numero : "))
-if numero > 0:
-    print("O número é positivo.")
-else: 
-    if  numero == 0:
-        print("O número é zero portando não é positivo nem negativo e sim neutro.")
-    else:
-        print("O número não é positivo!")
+def verifica_num():
+    while True:
+        numero = float(input("Digite um numero : "))
+        if numero > 0:
+            print("O número é positivo.")
+        else: 
+            if numero == 0:
+                print("O número é zero portando não é positivo nem negativo e sim neutro.")
+            else:
+                print("O número não é positivo!")
+        continua  = input("Deseja continuar executando?[sim, nao]: ").lower()
+        if continua == "nao":
+            break
 
-
-
-
+verifica_num()
 
