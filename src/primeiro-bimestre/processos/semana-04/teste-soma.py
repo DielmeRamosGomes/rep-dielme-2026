dc823a466350ac9cc7b8823a58996ef8bd439829
DielmeRamosGomes <dielme.ramos@unesp.br>
2026-03-25 12:19:12 -0300

    llp

--- src/primeiro-bimestre/processos/semana-04/teste-soma.py
+++ src/primeiro-bimestre/processos/semana-04/teste-soma.py
@@ -5,8 +5,8 @@ def soma(a, b):
 
 class TesteCaluladora(unittest.TestCase):
     def test_soma(self):
-        self.assertEqual(soma(2, 3), 5)
-        #self.assertNotEqual(soma(2, 2), 5)
+        #self.assertEqual(soma(2, 3), 5)
+        self.assertNotEqual(soma(2, 2), 5)
         
 if __name__=="__main__":
     unittest.main()
