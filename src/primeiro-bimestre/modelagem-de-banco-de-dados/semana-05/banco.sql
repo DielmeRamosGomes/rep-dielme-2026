22b020ede7405ee99638c3acf80b1827a2dd221e
DielmeRamosGomes <dielme.ramos@unesp.br>
2026-03-25 07:54:49 -0300

    md

--- src/primeiro-bimestre/modelagem-de-banco-de-dados/semana-05/banco.sql
+++ src/primeiro-bimestre/modelagem-de-banco-de-dados/semana-05/banco.sql
@@ -2,7 +2,8 @@ create database db_amazon;
 
 create table db_amazon.clientes(
     cod_cliente int auto_increment primary key,
-    nome varchar(100) not null
+    nome varchar(100) not null,
+    
 );
 
 
