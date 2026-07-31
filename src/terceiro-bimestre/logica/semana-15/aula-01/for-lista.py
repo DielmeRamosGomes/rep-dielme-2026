7462c9886e39a90d1beb20c08ef106671c53e456
DielmeRamosGomes <dielme.ramos@unesp.br>
2026-07-31 07:37:00 -0300

    for listas

---
+++ src/terceiro-bimestre/logica/semana-15/aula-01/for-lista.py
@@ -0,0 +1,16 @@
+lista  = [1, 2, 3, 4, 5]
+
+'''
+for i in range(0, len(lista)):
+    print(lista[i])
+'''
+
+'''
+for i in range(len(lista)):
+    print(lista[i])
+'''
+
+'''
+for i in range(len(lista)-1, -1, -1):
+    print(lista[i])
+'''
---
+++ src/terceiro-bimestre/logica/semana-15/aula-01/for.py
@@ -0,0 +1,20 @@
+'''
+for i in range(1, 6):
+    print(i)
+'''
+
+'''
+for i in range(6):
+    print(i)
+'''
+
+'''
+for i in range(0, 10, 2):
+    print(i)
+'''
+
+'''for i in range(10, -1, -1):
+    print(i)
+else:
+    print("Lançar!")
+'''
