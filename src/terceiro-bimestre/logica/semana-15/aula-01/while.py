5d0090ad41cc076a12a46beb032960bc841a9432
DielmeRamosGomes <dielme.ramos@unesp.br>
2026-07-31 10:30:33 -0300

    while

---
+++ src/terceiro-bimestre/logica/semana-15/aula-01/while.py
@@ -0,0 +1,27 @@
+'''
+index = 0
+while index <= 10:
+    print(index)
+    index += 1
+'''
+
+'''
+i = 10
+while i >= 0:
+    print(i)
+    i -= 1
+else:
+    print("Lançando foguete")
+'''
+
+'''
+lista = []
+
+while True:
+    fruta = input("Digite o nome fruta: ")
+    lista.append(fruta)
+    print(lista)
+    continua = input("Deseja sair? [s, n]: ").lower()
+    if (continua == "n") or (continua == "nao"):
+        break
+'''
