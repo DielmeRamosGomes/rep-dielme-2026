3e4db172a019d710ae3a559b5cc157114335516b
DielmeRamosGomes <dielme.ramos@unesp.br>
2026-08-04 20:20:26 -0300

    binario

--- src/terceiro-bimestre/redes-de-computadores/semana-16/decimal-binario.py
+++ src/terceiro-bimestre/redes-de-computadores/semana-16/decimal-binario.py
@@ -1,12 +1,12 @@
+from collections import deque
 resto = 0
-binario = []
+binario = deque([])
 quociente = int(input("Digite um número decimal: "))
 while quociente > 0:
     resto = quociente % 2
-    binario.append(resto)
+    binario.appendleft(resto)
     quociente = quociente // 2
-
-binario.reverse()
+    
 print("O número em binário é: ", end="")
 for bit in binario:
     print(bit, end="")
