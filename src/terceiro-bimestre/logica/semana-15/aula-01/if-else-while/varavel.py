3fd2dcd5ff80d7ffe633bd274daab2cd39e2556f
DielmeRamosGomes <dielme.ramos@unesp.br>
2026-08-04 10:43:47 -0300

    if

---
+++ src/terceiro-bimestre/logica/semana-15/aula-01/if-else-while/varavel.py
@@ -0,0 +1,13 @@
+'''
+idade = 18
+idade2 = 15
+print(f"Idade: {idade} e {idade2}")
+print(type(idade))
+'''
+
+primero_nome = "Miguel"
+print(f"Nome: {primero_nome}")
+print(type(primero_nome))
+
+
+
