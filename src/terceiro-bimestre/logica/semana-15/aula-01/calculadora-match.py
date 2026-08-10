7027634bade41731671129af7a5a24e464415c54
DielmeRamosGomes <dielme.ramos@unesp.br>
2026-08-10 09:31:01 -0300

    match

---
+++ src/terceiro-bimestre/logica/semana-15/aula-01/calculadora-match.py
@@ -0,0 +1,18 @@
+num1 = float(input("Digite um numero: "))
+num2 = float(input("Digite um numero: "))
+operação = input("Digite uma operação (+, -, *, /)")
+
+match operação:
+    case "+":
+        print(f"{num1} + {num2} = {num1 + num2}")
+    case "-":
+        print(f"{num1} - {num2} = {num1 - num2}")
+    case "*":
+        print(f"{num1} * {num2} = {num1 * num2}")
+    case "/":
+        if num2 > 0:
+            print(f"{num1} / {num2} = {num1 / num2}")
+        else:
+            print("Não é possivel dividir por zero!")
+    case _:
+        print("Operador inválido!")
