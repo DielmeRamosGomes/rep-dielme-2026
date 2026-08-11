11784f48a7f57639c47e548588968741dffeddf1
DielmeRamosGomes <dielme.ramos@unesp.br>
2026-08-11 10:53:43 -0300

    dia da semana

--- src/terceiro-bimestre/logica/semana-15/aula-01/dia-semana.py
+++ src/terceiro-bimestre/logica/semana-15/aula-01/dia-semana.py
@@ -1,4 +1,5 @@
-dia_semana = "Domingo"
+dia_semana = input("Digite um dia da semana: ")
+
 if dia_semana == "Domingo":
     print("Evento de Domingo")
 elif dia_semana == "Sábado":
@@ -13,4 +14,6 @@ elif dia_semana == "Quinta":
     print("Evento de Quinta")
 elif dia_semana == "Sexta":
     print("Evento de Sexta")
-
+else:
+    print("Digite um nome da semana")
+    
