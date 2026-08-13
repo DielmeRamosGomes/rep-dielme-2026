fee1c75afa1cce481b93b29d2016b447af17b095
DielmeRamosGomes <dielme.ramos@unesp.br>
2026-08-13 20:20:04 -0300

    match

--- src/terceiro-bimestre/logica/semana-15/aula-01/if-else-while/for.py
+++ src/terceiro-bimestre/logica/semana-15/aula-01/if-else-while/for.py
@@ -8,9 +8,10 @@ for i in range(6):
     print(i)
 '''
 
-for i in range(0, 10, 2):
+'''
+for i in range(0, 12, 2):
     print(i)
-
+'''
 
 '''for i in range(10, -1, -1):
     print(i)
--- src/terceiro-bimestre/logica/semana-15/aula-01/verifica-idade-match.py
+++
@@ -1,8 +0,0 @@
-idade = int(input("Digite uma idade: "))
-
-match idade:
-    case idade <= 4:
-    
-    case _:
-        print("Idade não pode ser negativo")   
-
