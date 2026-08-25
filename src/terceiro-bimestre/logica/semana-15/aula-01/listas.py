f5a87ccb7ca037360e7d9b193a175ba78d995e6a
DielmeRamosGomes <dielme.ramos@unesp.br>
2026-08-25 10:40:36 -0300

    logica

--- src/terceiro-bimestre/logica/semana-15/aula-01/listas.py
+++ src/terceiro-bimestre/logica/semana-15/aula-01/listas.py
@@ -2,14 +2,15 @@
 lista = [7.5, 8.0, 6.5, 9.0, 7.0]
 print(lista)
 '''
-'''
+
+
 numeros = []
 numeros.append(1)
 numeros.append(2)
 numeros.append(3)
 numeros.append(4)
 numeros.append(5)
-print(numeros)
-'''
+print(numeros[2])
+
 
 
