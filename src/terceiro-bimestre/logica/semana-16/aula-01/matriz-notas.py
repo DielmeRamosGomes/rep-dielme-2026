b7172baf489f152f5ba5aba9f4976820d38fcaeb
DielmeRamosGomes <dielme.ramos@unesp.br>
2026-08-30 23:13:07 -0300

    modelagem

---
+++ src/terceiro-bimestre/logica/semana-16/aula-01/matriz-notas.py
@@ -0,0 +1,13 @@
+matriz = [
+    ["Luis", 16],
+    ["João", 15],
+    ["Carla", 17],
+]
+
+for linha in range(3):
+    for coluna in range(2):
+        print(matriz[linha][coluna], end=" ")
+    print()
+
+
+
