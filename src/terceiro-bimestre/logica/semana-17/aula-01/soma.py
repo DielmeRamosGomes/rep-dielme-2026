790c13e5f647a5d5fd2df2bc95439cd21d410510
DielmeRamosGomes <dielme.ramos@unesp.br>
2026-09-08 10:37:52 -0300

    logica

---
+++ src/terceiro-bimestre/logica/semana-17/aula-01/soma.py
@@ -0,0 +1,9 @@
+def soma(num1, num2):
+    return num1 + num2
+
+num1 = float(input("Digite um valor1: "))
+num2 = float(input("Digite um valor2: "))
+
+resultado = soma(num1, num2)
+print(f"{num1} + {num2} = {resultado}")
+#print(f"{num1} + {num2} = {soma(num1, num2)}")
