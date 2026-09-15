a6bffcfaf4a1375d3fd095b0065d896c74d807d1
DielmeRamosGomes <dielme.ramos@unesp.br>
2026-09-15 08:30:18 -0300

    modelagem

---
+++ src/terceiro-bimestre/modelagem/semana-18/aula-03/s18a3.sql
@@ -0,0 +1,51 @@
+create database if not exists s18a3;
+
+CREATE TABLE s18a3.clientes(
+    id int PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(100) NOT NULL,
+    cidade VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE s18a3.fornecedores(
+    id int PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(100) NOT NULL,
+    cidade VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL
+);
+
+INSERT INTO  s18a3.clientes(nome, cidade, email) 
+VALUES("João Silva", "São Paulo", "joao@email.com"),
+("Maria Santos", "Rio de Janeiro", "maria@email.com"),
+("Pedro Lima", "São Paulo", "pedro@email.com");
+
+INSERT INTO  s18a3.fornecedores(nome, cidade, email) 
+VALUES("João Silva", "São Paulo", "joao@fornece.com"),
+("Ana Costa", "Salvador", "ana@fornece.com"),
+("Carlos Nunes", "Recife", "carlos@fornece.com");
+
+-- 1) 
+SELECT nome FROM clientes
+UNION
+SELECT nome FROM fornecedores;
+
+-- 2) 
+SELECT nome FROM clientes
+UNION ALL
+SELECT nome FROM fornecedores;
+
+--3) 
+SELECT nome, e-mail FROM clientes
+UNION ALL
+SELECT nome, email FROM fornecedores;
+
+--4)
+SELECT nome, COUNT(*) AS total
+FROM (
+    SELECT nome FROM clientes
+    UNION ALL
+    SELECT nome FROM fornecedores
+) AS nomes_combinados
+GROUP BY nome
+HAVING COUNT(*) > 1;
+
