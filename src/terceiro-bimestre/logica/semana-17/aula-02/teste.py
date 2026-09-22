83c6400dc81c624582ddf3e8f000abc4db6f516e
DielmeRamosGomes <dielme.ramos@unesp.br>
2026-09-22 11:15:31 -0300

    llp

--- src/terceiro-bimestre/logica/semana-17/aula-02/teste.py
+++ src/terceiro-bimestre/logica/semana-17/aula-02/teste.py
@@ -1,26 +1,16 @@
-import random
+carros = []
 
-def im_matriz1(matriz1):
-    dim_linha = len(matriz1)
-    dim_coluna = len(matriz1[0])
-    for lin in range(dim_linha):
-        for col in range(dim_coluna):
-            print(matriz1[lin][col], end=" ")
-        print()
-matriz1 = [[random.randint(1,10) for col in range(3)] for lin in range(3)]
-print("matriz1:",matriz1)
+while True:
+    
+    carros.append(input("\nDigite o nome do carro: "))
+    
+    
+    print("\n--- Lista Atualizada ---")
+    for i, carro in enumerate(carros, 1):
+        print(f"{i}º - {carro}")
+        
 
-def im_matriz2(matriz2):
-    dim_linha = len(matriz2)
-    dim_coluna = len(matriz2[0])
-    for lin in range(dim_linha):
-        for col in range(dim_coluna):
-            print(matriz2[lin][col], end=" ")
-        print()
-matriz2 = [[random.randint(1,10) for col in range(3)] for lin in range(3)]
-print("matriz2:",matriz2)
+    if input("\nQuer continuar? (s/n): ").lower() != 's':
+        break
 
-soma = matriz1
-total = matriz2
-total += soma
-print(f"soma é:",total)
+print("\nFim do programa!")
